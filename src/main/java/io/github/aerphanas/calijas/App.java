7fea150f9e12f4e3bc9eef486b53734ff0e5857e
Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>
2022-12-27 15:12:42 +0700

    add app
    Signed-off-by: Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>

--- src/main/java/io/github/aerphanas/calijas/App.java
+++ src/main/java/io/github/aerphanas/calijas/App.java
@@ -1,13 +1,77 @@
 package io.github.aerphanas.calijas;
 
-/**
- * Hello world!
- *
- */
+import org.apache.commons.cli.*;
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+  public static void main( String[] args ) {
+    Options pilihan = new Options();
+
+    Option variablex = new Option("x", "varx", true, "tolong masukan variabel x");
+    variablex.setRequired(true);
+    pilihan.addOption(variablex);
+
+    Option variabley = new Option("y", "vary", true, "tolong masukan variabel y");
+    variabley.setRequired(true);
+    pilihan.addOption(variabley);
+
+    Option operator = new Option("o", "operator", true, "tolong masukan operator");
+    operator.setRequired(true);
+    pilihan.addOption(operator);
+
+    CommandLineParser parser = new DefaultParser();
+    HelpFormatter formatter = new HelpFormatter();
+    CommandLine cmd = null;
+
+    try {
+      cmd = parser.parse(pilihan, args);
+    } catch (ParseException e) {
+      System.out.println(e.getMessage());
+      formatter.printHelp("caljas", pilihan);
+      System.exit(1);
     }
+
+    String inputVary = cmd.getOptionValue("vary");
+    String inputVarx = cmd.getOptionValue("varx");
+    String inputOperator = cmd.getOptionValue("operator");
+
+    int varx = 0;
+    int vary = 0;
+    int result = 0;
+
+    String [] symbol = {"+", "-", "*", "/"};
+    try {
+      varx = Integer.parseInt(inputVarx);
+      vary = Integer.parseInt(inputVary);
+    } catch (NumberFormatException e) {
+      e.printStackTrace();
+    }
+
+    Calculator docalc = new Caljas(varx, vary);
+
+    for (int i = 0; i < symbol.length; i++) {
+      if ( inputOperator.equals(symbol[i]) ){
+        switch (inputOperator) {
+          case "+":
+            result = docalc.add();
+            break;
+          case "-":
+            result = docalc.min();
+            break;
+          case "*":
+            result = docalc.mul();
+            break;
+          case "/":
+            result = docalc.div();
+            break;
+        }
+      } else {
+        System.err.println("program saatini hanya mensupport + - * /");
+        System.exit(1);
+        break;
+      }
+    }  
+    System.out.printf("hasil %d %s %d adalah %d", varx, inputOperator, vary, result);
+  }
 }
+
